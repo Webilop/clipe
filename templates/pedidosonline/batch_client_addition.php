067c9100489bc41f886881f22e148b4e820bc2c8
diegofvo <diegofvo@gmail.com>
2015-04-23 10:31:04 -0500

    validate type of file client upload.

--- templates/pedidosonline/batch_client_addition.php
+++ templates/pedidosonline/batch_client_addition.php
@@ -3,14 +3,13 @@ global $pedidosOnline;
 $pedidosOnline->is_login(true);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   //show message is uploading.exit;
-  $result = $pedidosOnline->addition_file_of_clients(); 
+  $result = $pedidosOnline->addition_file_of_clients();
   if (isset($result->status) && $result->status == "success") {
     $pedidosOnline->add_flash_message(__($result->message, 'clipe'), 'success');
-  }
-  else {
+  } else {
     foreach ($result->errors as $error) {
-     $pedidosOnline->add_flash_message($error); 
-    }    
+      $pedidosOnline->add_flash_message($error);
+    }
   }
 }
 get_header();
@@ -19,7 +18,7 @@ get_header();
   <h1><?php _e('Batch Client Addition', 'clipe'); ?></h1>
   <form method="POST" enctype="multipart/form-data">
     <div>
-      <label for="address"><?php _e('File', 'clipe'); ?></label>
+      <label for="address"><?php _e('File', 'clipe'); ?> <span title="<?php echo __('supported file: xls, xlsx, xlsm, csv or ods.','clipe');?>"><i class="fa fa-info-circle"></i></span></label>
       <input type="file" id="file" name="file" required/>
     </div>    
     <input type="submit" value="<?php _e('Upload', 'clipe'); ?>" class="" id="submit" >
@@ -29,6 +28,21 @@ get_header();
     <a href="<?php echo $pedidosOnline->get_link_page('index.php'); ?>"><i class="fa fa-home"></i></a>
     <a href="<?php echo $pedidosOnline->get_logout_url(); ?>"><i class="fa fa-sign-out"></i></a>
   </div>
+  <script type="text/javascript">
+    window.onload = function () {
+      document.getElementById("file").onchange = requiredFile;
+    }
+    
+    function requiredFile(){
+      ext=jQuery('#file').val().split('.').pop().toLowerCase();
+      if(jQuery.inArray(ext, ['xls','xlsx','xlsm','csv','ods']) == -1) {
+          document.getElementById("file").setCustomValidity("<?php _e("the file is not supported, check it xls, xlsx, xlsm, csv or ods.", "clipe"); ?>");
+      }else{
+        document.getElementById("file").setCustomValidity('');
+      }      
+    }
+
+  </script>
 </div>
 <?php
 get_sidebar('clipe');
