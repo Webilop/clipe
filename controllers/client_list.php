d28932405b1b372dff9c7d5d59d97e1302d71b47
Andrés Ricardo Torres Martínez <torresandresricardo@gmail.com>
2015-10-26 16:07:12 -0500

    Add clients from file: show messages

--- controllers/client_list.php
+++ controllers/client_list.php
@@ -5,8 +5,7 @@ $pedidosOnline->validatePermission('provider');
 $active = isset($_GET['page']) ? $_GET['page'] : 1;
 $numberRows=10;
 $result = $pedidosOnline->get_clients(array('page'=>$active,'limit' => $numberRows, 'order_by' => 'name'));
-$clients = $result->clients;
+
+$clients = isset($result->clients) && is_array($result->clients)? $result->clients : array();
 
 $createClientUrl = $pedidosOnline->get_link_page("client_create.php");
-
-?>
